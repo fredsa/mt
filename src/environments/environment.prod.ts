e784e2f2284fbd25569aa4ad96bd29c08cd3951d
Fred Sauer <fredsa@gmail.com>
2019-02-19 20:58:15 -0800

    prod env that uses dev env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+
+  firebase: {
+    apiKey: 'AIzaSyDRMejc4IawHdjhj0g0Ys3t1eYPWD6uGzs',
+    authDomain: 'teslatweaks.firebaseapp.com',
+    databaseURL: 'https://teslatweaks.firebaseio.com',
+    projectId: 'teslatweaks',
+    storageBucket: 'teslatweaks.appspot.com',
+    messagingSenderId: '640450368740'
+  },
+
 };
